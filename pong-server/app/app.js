5c9a46852a15d82014db3b5358cbcec136911bae
Gustav Svensk <gustav.svensk95@gmail.com>
2021-02-15 17:58:11 +0100

    add pong

---
+++ pong-server/app/app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+
+module.exports.run = () => {
+  const app = express();
+
+  app.get('/', (_, res) => {
+    res.send('hello world');
+  });
+
+  app.get('/pong', (req, res) => {
+    console.log("ponged")
+    setTimeout(() => {
+      http.get('http://localhost:3000/ping');
+      res.send('ping pong has begun!');
+    }, 5000);
+  });
+
+  const server = http.createServer(app);
+
+  const port = 3001;
+  server.listen(port, () =>
+    process.stdout.write(`Pong Server is listening on port ${port}\n`)
+  );
+};
